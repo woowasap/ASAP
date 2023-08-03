a53e519c0042e437481082c028211f9e1831dd7d
SeoSeunghun <proto_seo@naver.com>
2023-08-03 10:35:38 +0900

    chore: `settings.gradle`의 rootProject.name 변경 (#22)

--- app/src/main/java/shop/woowasap/Application.java
+++ app/src/main/java/shop/woowasap/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        new SpringApplication().run(args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
