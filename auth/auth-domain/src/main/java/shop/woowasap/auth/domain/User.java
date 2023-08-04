8e76245a3bbdd04825034eb0bd9112788bf4656f
김명준 <audwnswmf@naver.com>
2023-08-04 16:00:20 +0900

    refactor: User 도메인 객체 단순화

--- auth/auth-domain/src/main/java/shop/woowasap/auth/domain/User.java
+++ auth/auth-domain/src/main/java/shop/woowasap/auth/domain/User.java
@@ -1,19 +1,14 @@
 package shop.woowasap.auth.domain;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 
 @Getter
-@ToString
-@EqualsAndHashCode
 @RequiredArgsConstructor
 public final class User {
 
     private final Long id;
-    private final String nickname;
-    private final String username;
+    private final String userId;
     private final String password;
 
 }
