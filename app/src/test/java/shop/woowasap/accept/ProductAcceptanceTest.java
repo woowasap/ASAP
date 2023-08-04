f7f90da1bd7d5b2836ff4176f5ae0fd327a7bca5
SeoSeunghun <proto_seo@naver.com>
2023-08-04 16:32:06 +0900

    test: 상품 등록 인수테스트 작성 (#37)
    Co-authored-by: xb205 <62425964+devxb@users.noreply.github.com>

---
+++ app/src/test/java/shop/woowasap/accept/ProductAcceptanceTest.java
@@ -0,0 +1,52 @@
+package shop.woowasap.accept;
+
+import static shop.woowasap.accept.product.ProductFixture.forbiddenUserLoginRequest;
+import static shop.woowasap.accept.product.ProductFixture.loginRequest;
+import static shop.woowasap.accept.product.ProductFixture.registerProductRequest;
+import static shop.woowasap.accept.support.valid.HttpValidator.assertForbidden;
+import static shop.woowasap.accept.support.valid.ShopValidator.assertProductRegistered;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import shop.woowasap.accept.support.api.AuthApiSupporter;
+import shop.woowasap.accept.support.api.ShopApiSupporter;
+import shop.woowasap.mock.dto.LoginRequest;
+import shop.woowasap.mock.dto.LoginResponse;
+
+@DisplayName("상품 인수테스트")
+class ProductAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    @DisplayName("POST /products 요청을 통해서 상품을 생성한다.")
+    void createProduct() {
+        // given
+        final LoginRequest loginRequest = loginRequest();
+        final String accessToken = AuthApiSupporter.login(loginRequest)
+            .as(LoginResponse.class).token();
+
+        // when
+        final ExtractableResponse<Response> response = ShopApiSupporter
+            .registerProduct(accessToken, registerProductRequest());
+
+        // then
+        assertProductRegistered(response);
+    }
+
+    @Test
+    @DisplayName("권한이 없는 회원이 /products 요청을 통해서 상품을 생성하는 경우 FORBIDDEN 응답을 받는다.")
+    void createProductWithForbidden() {
+        // given
+        final LoginRequest forbiddenUserLoginRequest = forbiddenUserLoginRequest();
+        final String accessToken = AuthApiSupporter.login(forbiddenUserLoginRequest)
+            .as(LoginResponse.class).token();
+
+        // when
+        final ExtractableResponse<Response> response = ShopApiSupporter
+            .registerProduct(accessToken, registerProductRequest());
+
+        // then
+        assertForbidden(response);
+    }
+}
