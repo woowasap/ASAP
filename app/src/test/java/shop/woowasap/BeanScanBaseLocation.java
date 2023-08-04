32d2fd9441d1bbd8f7323586fe930c62a163c09f
xb205 <62425964+devxb@users.noreply.github.com>
2023-08-04 10:21:18 +0900

    test: 인수테스트 템플릿 추가 (#35)

---
+++ app/src/test/java/shop/woowasap/BeanScanBaseLocation.java
@@ -0,0 +1,5 @@
+package shop.woowasap;
+
+public class BeanScanBaseLocation {
+
+}
---
+++ app/src/test/java/shop/woowasap/accept/support/valid/ShopValidator.java
@@ -0,0 +1,56 @@
+package shop.woowasap.accept.support.valid;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.List;
+import shop.woowasap.mock.dto.ProductsResponse;
+import shop.woowasap.mock.dto.ProductsResponse.Product;
+
+public final class ShopValidator {
+
+    private ShopValidator() {
+    }
+
+    public static void assertProductRegistered(ExtractableResponse<Response> result) {
+        HttpValidator.assertCreated(result);
+
+        assertThat(result.header("Location")).isNotBlank();
+    }
+
+    public static void assertProductUpdated(ExtractableResponse<Response> result) {
+        HttpValidator.assertOk(result);
+    }
+
+    public static void assertProductsFound(ExtractableResponse<Response> result,
+        ProductsResponse expected) {
+        HttpValidator.assertOk(result);
+
+        ProductsResponse resultResponse = result.as(ProductsResponse.class);
+        assertProducts(resultResponse, expected);
+    }
+
+    private static void assertProducts(ProductsResponse result, ProductsResponse expected) {
+        assertThat(result.page()).isEqualTo(expected.page());
+        assertThat(result.totalPage()).isEqualTo(expected.totalPage());
+
+        assertProductsExceptId(result, expected);
+    }
+
+    private static void assertProductsExceptId(ProductsResponse result, ProductsResponse expected) {
+        List<Product> resultList = result.products();
+        List<ProductsResponse.Product> expectedList = expected.products();
+        assertThat(resultList).hasSize(expectedList.size());
+
+        for (int i = 0; i < resultList.size(); i++) {
+            ProductsResponse.Product resultElement = resultList.get(i);
+            ProductsResponse.Product expectedElement = expectedList.get(i);
+
+            assertThat(resultElement.name()).isEqualTo(expectedElement.name());
+            assertThat(resultElement.price()).isEqualTo(expectedElement.price());
+            assertThat(resultElement.endTime()).isEqualTo(expectedElement.endTime());
+            assertThat(resultElement.startTime()).isEqualTo(expectedElement.startTime());
+        }
+    }
+}
