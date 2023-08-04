1c8313e95a27d153dba86f1187ea75e53d9feef6
pushedrumex <pushedrumex@naver.com>
2023-08-04 16:22:55 +0900

    test: 상품수정 인수테스트 작성

---
+++ app/src/test/java/shop/woowasap/accept/ProductAcceptanceTest.java
@@ -0,0 +1,53 @@
+package shop.woowasap.accept;
+
+import static shop.woowasap.accept.product.ProductFixture.forbiddenUserLoginRequest;
+import static shop.woowasap.accept.product.ProductFixture.loginRequest;
+import static shop.woowasap.accept.product.ProductFixture.updateProductRequest;
+import static shop.woowasap.accept.support.valid.HttpValidator.assertForbidden;
+import static shop.woowasap.accept.support.valid.HttpValidator.assertOk;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import shop.woowasap.accept.support.api.AuthApiSupporter;
+import shop.woowasap.accept.support.api.ShopApiSupporter;
+import shop.woowasap.mock.dto.LoginRequest;
+import shop.woowasap.mock.dto.LoginResponse;
+
+@DisplayName("Product 인수테스트")
+public class ProductAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    @DisplayName("상품 내용을 수정한다.")
+    void updateProduct() {
+        // given
+        final LoginRequest loginRequest = loginRequest();
+        final String accessToken = AuthApiSupporter.login(loginRequest)
+            .as(LoginResponse.class).token();
+
+        // when
+        ExtractableResponse<Response> response = ShopApiSupporter.updateProduct(accessToken,
+            updateProductRequest());
+
+        // then
+        assertOk(response);
+    }
+
+    @Test
+    @DisplayName("권한이 없는 회원이 상품을 수정하는 경우 FORBIDDEN 응답을 받는다.")
+    void updateProductWithForbidden() {
+        // given
+        final LoginRequest forbiddenUserLoginRequest = forbiddenUserLoginRequest();
+        final String accessToken = AuthApiSupporter.login(forbiddenUserLoginRequest)
+            .as(LoginResponse.class).token();
+
+        // when
+        ExtractableResponse<Response> response = ShopApiSupporter.updateProduct(accessToken,
+            updateProductRequest());
+
+        // then
+        assertForbidden(response);
+    }
+
+}
