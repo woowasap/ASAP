da9318c23547552b19dc8e2f7aada858a2dd529f
grolarkim <audwnswmf@naver.com>
2023-08-05 16:24:00 +0900

    feat: 유저 도메인 생성 (#43)
    Co-authored-by: xb205 <62425964+devxb@users.noreply.github.com>

--- auth/auth-domain/src/main/java/shop/woowasap/auth/domain/User.java
+++ auth/auth-domain/src/main/java/shop/woowasap/auth/domain/User.java
@@ -1,19 +1,33 @@
 package shop.woowasap.auth.domain;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import lombok.Builder;
 
-@Getter
-@ToString
-@EqualsAndHashCode
-@RequiredArgsConstructor
 public final class User {
 
     private final Long id;
-    private final String nickname;
-    private final String username;
-    private final String password;
+    private final UserId userId;
+    private final Password password;
 
+    @Builder
+    public User(Long id, String userId, String password) {
+        this.id = id;
+        this.userId = UserId.of(userId);
+        this.password = Password.of(password);
+    }
+
+    public void assertNotDuplicatedUserId(User user) {
+        this.userId.assertNotDuplicated(user.userId);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId.getValue();
+    }
+
+    public String getPassword() {
+        return password.getValue();
+    }
 }
---
+++ auth/auth-domain/src/main/java/shop/woowasap/auth/domain/exception/PasswordValidateException.java
@@ -0,0 +1,8 @@
+package shop.woowasap.auth.domain.exception;
+
+public final class PasswordValidateException extends AuthDomainBaseException {
+
+    public PasswordValidateException(String message) {
+        super(message);
+    }
+}
