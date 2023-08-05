1146ff7661a8a231da4c03fd04eb35c8bf664e71
pushedrumex <pushedrumex@naver.com>
2023-08-05 13:53:19 +0900

    fix: 패키지 이동 관련 오류 해결

--- app/src/test/java/shop/woowasap/accept/ProductAcceptanceTest.java
+++ app/src/test/java/shop/woowasap/accept/ProductAcceptanceTest.java
@@ -14,7 +14,7 @@ import shop.woowasap.accept.support.api.AuthApiSupporter;
 import shop.woowasap.accept.support.api.ShopApiSupporter;
 import shop.woowasap.mock.dto.LoginRequest;
 import shop.woowasap.mock.dto.LoginResponse;
-import shop.woowasap.mock.dto.UpdateProductRequest;
+import shop.woowasap.shop.service.dto.UpdateProductRequest;
 
 @DisplayName("Product 인수테스트")
 class ProductAcceptanceTest extends AcceptanceTest {
