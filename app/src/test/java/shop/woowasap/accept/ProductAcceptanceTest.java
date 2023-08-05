58190bd14febdb35c8e951845731c68a6a5cdcad
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-08-05 21:46:33 +0900

    test: 상품 수정 인수테스트 수정 및 추가 (#44)

--- app/src/test/java/shop/woowasap/accept/ProductAcceptanceTest.java
+++ app/src/test/java/shop/woowasap/accept/ProductAcceptanceTest.java
@@ -2,18 +2,22 @@ package shop.woowasap.accept;
 
 import static shop.woowasap.accept.product.ProductFixture.forbiddenUserLoginRequest;
 import static shop.woowasap.accept.product.ProductFixture.loginRequest;
+import static shop.woowasap.accept.product.ProductFixture.registerProductRequest;
 import static shop.woowasap.accept.product.ProductFixture.updateProductRequest;
+import static shop.woowasap.accept.support.api.AuthApiSupporter.login;
+import static shop.woowasap.accept.support.api.ShopApiSupporter.registerProduct;
 import static shop.woowasap.accept.support.valid.HttpValidator.assertForbidden;
+import static shop.woowasap.accept.support.valid.HttpValidator.assertNotFound;
 import static shop.woowasap.accept.support.valid.HttpValidator.assertOk;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import shop.woowasap.accept.support.api.AuthApiSupporter;
 import shop.woowasap.accept.support.api.ShopApiSupporter;
 import shop.woowasap.mock.dto.LoginRequest;
 import shop.woowasap.mock.dto.LoginResponse;
+import shop.woowasap.mock.dto.RegisterProductRequest;
 import shop.woowasap.mock.dto.UpdateProductRequest;
 
 @DisplayName("Product 인수테스트")
@@ -24,12 +28,22 @@ class ProductAcceptanceTest extends AcceptanceTest {
     void updateProduct() {
         // given
         final LoginRequest loginRequest = loginRequest();
-        final String accessToken = AuthApiSupporter.login(loginRequest)
+        final String accessToken = login(loginRequest)
             .as(LoginResponse.class).token();
+
+        final RegisterProductRequest registerProductRequest = registerProductRequest();
+        final ExtractableResponse<Response> registerResponse = registerProduct(accessToken,
+            registerProductRequest);
+
+        final long productId = Long.parseLong(registerResponse
+                            .header("Location")
+                            .split("/")[4]);
+
         final UpdateProductRequest updateProductRequest = updateProductRequest();
 
         // when
         ExtractableResponse<Response> response = ShopApiSupporter.updateProduct(accessToken,
+            productId,
             updateProductRequest);
 
         // then
@@ -41,16 +55,47 @@ class ProductAcceptanceTest extends AcceptanceTest {
     void updateProductWithForbidden() {
         // given
         final LoginRequest forbiddenUserLoginRequest = forbiddenUserLoginRequest();
-        final String accessToken = AuthApiSupporter.login(forbiddenUserLoginRequest)
+        final String accessToken = login(forbiddenUserLoginRequest)
             .as(LoginResponse.class).token();
+
+        final RegisterProductRequest registerProductRequest = registerProductRequest();
+        final ExtractableResponse<Response> registerResponse = registerProduct(accessToken,
+            registerProductRequest);
+
+        final long productId = Long.parseLong(registerResponse
+            .header("Location")
+            .split("/")[4]);
+
         final UpdateProductRequest updateProductRequest = updateProductRequest();
 
         // when
         ExtractableResponse<Response> response = ShopApiSupporter.updateProduct(accessToken,
+            productId,
             updateProductRequest);
 
         // then
         assertForbidden(response);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 상품을 수정하려고 하는경우 NOTFOUND 응답을 받는다.")
+    void updateProductWithNotFound() {
+        // given
+        final LoginRequest forbiddenUserLoginRequest = forbiddenUserLoginRequest();
+        final String accessToken = login(forbiddenUserLoginRequest)
+            .as(LoginResponse.class).token();
+
+        final long invalidProductId = 123;
+
+        final UpdateProductRequest updateProductRequest = updateProductRequest();
+
+        // when
+        ExtractableResponse<Response> response = ShopApiSupporter.updateProduct(accessToken,
+            invalidProductId,
+            updateProductRequest);
+
+        // then
+        assertNotFound(response);
     }
 
 }
