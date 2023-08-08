4129298959d44e44f5a200aa4a1379944a6594ac
pushedrumex <pushedrumex@naver.com>
2023-08-08 23:11:14 +0900

    fix: 수정된 Product.update 에 따른 test 코드를 수정한다

--- shop/shop-domain/src/test/java/shop/woowasap/domain/product/ProductTest.java
+++ shop/shop-domain/src/test/java/shop/woowasap/domain/product/ProductTest.java
@@ -1,14 +1,13 @@
 package shop.woowasap.domain.product;
 
-import static shop.woowasap.domain.support.ProductFixture.updateProductRequest;
 import static shop.woowasap.domain.support.DomainFixture.getDefaultBuilder;
 import static shop.woowasap.domain.support.ProductValidator.assertProduct;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import shop.woowasap.shop.domain.Product;
-import shop.woowasap.shop.dto.UpdateProductRequest;
 
 @DisplayName("Product 클래스")
 public class ProductTest {
@@ -21,14 +20,20 @@ public class ProductTest {
         @DisplayName("갱신된 Product 를 반환한다")
         void returnUpdatedProduct() {
             // given
-            final UpdateProductRequest updateProductRequest = updateProductRequest();
             final Product original = getDefaultBuilder().build();
 
+            final String name = "newProductName";
+            final String description = "newProductDescription";
+            final String price = "100";
+            final int quantity = 8;
+            final LocalDateTime startTime = LocalDateTime.of(2023, 8, 5, 11, 30);
+            final LocalDateTime endTime = LocalDateTime.of(2023, 9, 5, 14, 30);
+
             // when
-            Product update = original.update(original.getId(), updateProductRequest);
+            Product update = original.update(name, description, price, quantity, startTime, endTime);
 
             // then
-            assertProduct(original.getId(), updateProductRequest, update);
+            assertProduct(original.getId(), name, description, price, quantity, startTime, endTime, update);
         }
     }
 }
--- shop/shop-domain/src/test/java/shop/woowasap/domain/support/ProductValidator.java
+++ shop/shop-domain/src/test/java/shop/woowasap/domain/support/ProductValidator.java
@@ -1,9 +1,9 @@
 package shop.woowasap.domain.support;
 
+import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import org.assertj.core.api.SoftAssertions;
 import shop.woowasap.shop.domain.Product;
-import shop.woowasap.shop.dto.UpdateProductRequest;
 
 public class ProductValidator {
 
@@ -11,21 +11,27 @@ public class ProductValidator {
         throw new UnsupportedOperationException("Cannot invoke constructor \"ProductValidator()\"");
     }
 
-    public static void assertProduct(long id, UpdateProductRequest updateProductRequest, Product update) {
+    public static void assertProduct(
+        long id,
+        String name,
+        String description,
+        String price,
+        int quantity,
+        LocalDateTime startTime,
+        LocalDateTime endTime,
+        Product update
+    ) {
         SoftAssertions.assertSoftly(softAssertions -> {
-
             softAssertions.assertThat(update.getId()).isEqualTo(id);
-            softAssertions.assertThat(update.getName()).isEqualTo(updateProductRequest.name());
-            softAssertions.assertThat(update.getPrice()).isEqualTo(updateProductRequest.price());
-            softAssertions.assertThat(update.getQuantity()).isEqualTo(updateProductRequest.quantity());
-            softAssertions.assertThat(update.getDescription()).isEqualTo(updateProductRequest.description());
+            softAssertions.assertThat(update.getName()).isEqualTo(name);
+            softAssertions.assertThat(update.getDescription()).isEqualTo(description);
+            softAssertions.assertThat(update.getPrice()).isEqualTo(price);
+            softAssertions.assertThat(update.getQuantity()).isEqualTo(quantity);
 
             softAssertions.assertThat(update.getStartTime())
-                .isEqualTo(updateProductRequest.startTime()
-                    .atZone(ZoneOffset.UTC).toInstant());
+                .isEqualTo(startTime.atZone(ZoneOffset.UTC).toInstant());
             softAssertions.assertThat(update.getEndTime())
-                .isEqualTo(updateProductRequest.endTime()
-                    .atZone(ZoneOffset.UTC).toInstant());
+                .isEqualTo(endTime.atZone(ZoneOffset.UTC).toInstant());
         });
     }
 
