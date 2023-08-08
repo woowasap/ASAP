1f0f3541c39f3a9adea151e9c3983a66d9ab7d14
ProtoSeo <proto_seo@naver.com>
2023-08-08 11:43:39 +0900

    test: Product 생성 테스트 작성

---
+++ shop/shop-service/src/test/java/shop/woowasap/shop/service/ProductServiceTest.java
@@ -0,0 +1,52 @@
+package shop.woowasap.shop.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static shop.woowasap.shop.service.support.fixture.ProductFixture.productBuilder;
+import static shop.woowasap.shop.service.support.fixture.ProductFixture.registerProductRequest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import shop.woowasap.shop.domain.product.Product;
+import shop.woowasap.shop.service.dto.RegisterProductRequest;
+import shop.woowasap.shop.service.repository.ProductRepository;
+
+@ExtendWith(SpringExtension.class)
+@DisplayName("ProductService 클래스")
+@ContextConfiguration(classes = ProductService.class)
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    @Nested
+    @DisplayName("Product를 생성하는 메서드는")
+    class CreateProduct {
+
+        @Test
+        @DisplayName("정상적인 Request 입력 시, Product를 생성한다.")
+        void create() {
+            // given
+            final Long productId = 1L;
+            final RegisterProductRequest registerProductRequest = registerProductRequest();
+            final Product product = productBuilder(productId).build();
+            when(productRepository.save(any())).thenReturn(product);
+
+            // when
+            final Long resultProductId = productService.createProduct(registerProductRequest);
+
+            // then
+            assertThat(resultProductId).isEqualTo(productId);
+        }
+    }
+}
