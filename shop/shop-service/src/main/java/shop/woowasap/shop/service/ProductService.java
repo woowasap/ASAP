428b78cfde65188f5118ca76210a466f1d74d9b5
ProtoSeo <proto_seo@naver.com>
2023-08-09 16:28:59 +0900

    feat: IdGenerator 추가

--- shop/shop-service/src/main/java/shop/woowasap/shop/service/ProductService.java
+++ shop/shop-service/src/main/java/shop/woowasap/shop/service/ProductService.java
@@ -5,6 +5,7 @@ import static shop.woowasap.shop.service.mapper.ProductMapper.toDomain;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.woowasap.core.id.api.IdGenerator;
 import shop.woowasap.shop.domain.product.Product;
 import shop.woowasap.shop.service.dto.RegisterProductRequest;
 import shop.woowasap.shop.service.repository.ProductRepository;
@@ -14,10 +15,12 @@ import shop.woowasap.shop.service.repository.ProductRepository;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final IdGenerator idGenerator;
 
     @Transactional
     public Long registerProduct(final RegisterProductRequest registerProductRequest) {
-        final Product persistProduct = productRepository.save(toDomain(registerProductRequest));
+        final Product persistProduct = productRepository.save(
+            toDomain(idGenerator, registerProductRequest));
 
         return persistProduct.getId();
     }
