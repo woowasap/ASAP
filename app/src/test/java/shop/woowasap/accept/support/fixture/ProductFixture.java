abd09f145cb05f3c667d995ea9100a804047a837
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-08-10 18:36:16 +0900

    test: 상품 전체 조회 인수테스트 (#80)

--- app/src/test/java/shop/woowasap/accept/support/fixture/ProductFixture.java
+++ app/src/test/java/shop/woowasap/accept/support/fixture/ProductFixture.java
@@ -1,7 +1,11 @@
 package shop.woowasap.accept.support.fixture;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import shop.woowasap.mock.dto.LoginRequest;
+import shop.woowasap.mock.dto.ProductsResponse;
+import shop.woowasap.mock.dto.ProductsResponse.Product;
 import shop.woowasap.shop.app.api.request.RegisterProductRequest;
 
 public class ProductFixture {
@@ -15,6 +19,12 @@ public class ProductFixture {
     public static final long QUANTITY = 10L;
     public static final LocalDateTime START_TIME = LocalDateTime.of(2023, 8, 5, 12, 30);
     public static final LocalDateTime END_TIME = LocalDateTime.of(2023, 8, 5, 14, 30);
+    public static final LocalDateTime INFINITE_START_TIME = LocalDateTime.of(99_999, 12, 31, 23, 59);
+    public static final LocalDateTime INFINITE_END_TIME = LocalDateTime.of(999_999, 12, 31, 23, 59);
+    public static final long UNKNOWN_ID = 1L;
+    public static final int PAGE = 1;
+    public static final int TOTAL_PAGE = 1;
+
 
     public static LoginRequest loginRequest() {
         return new LoginRequest(USER_ID, PASSWORD);
@@ -26,5 +36,25 @@ public class ProductFixture {
 
     public static RegisterProductRequest registerProductRequest() {
         return new RegisterProductRequest(NAME, DESCRIPTION, PRICE, QUANTITY, START_TIME, END_TIME);
+    }
+
+    public static RegisterProductRequest registerValidProductRequest() {
+        return new RegisterProductRequest(NAME, DESCRIPTION, PRICE, QUANTITY, INFINITE_START_TIME, INFINITE_END_TIME);
+    }
+
+    public static RegisterProductRequest registerInvalidProductRequest() {
+        return new RegisterProductRequest(NAME, DESCRIPTION, PRICE, QUANTITY, LocalDateTime.now().minusHours(3), LocalDateTime.now().minusHours(2));
+    }
+
+    public static ProductsResponse productsResponse(final List<RegisterProductRequest> registerProductRequests) {
+        final List<Product> products = registerProductRequests.stream().map(product -> new Product(
+            UNKNOWN_ID,
+            product.name(),
+            Long.valueOf(product.price()),
+            product.startTime(),
+            product.endTime()
+        )).collect(Collectors.toList());
+
+        return new ProductsResponse(products, PAGE, TOTAL_PAGE);
     }
 }
