8a5205dd241d3a70b3163ea74a1df61b6a804e27
SeoSeunghun <proto_seo@naver.com>
2023-08-10 16:16:23 +0900

    feat: 어드민 상품 목록 조회 서비스 구현 (#81)

--- shop/shop-service/src/test/java/shop/woowasap/shop/service/support/fixture/ProductFixture.java
+++ shop/shop-service/src/test/java/shop/woowasap/shop/service/support/fixture/ProductFixture.java
@@ -1,7 +1,11 @@
 package shop.woowasap.shop.service.support.fixture;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.List;
+import shop.woowasap.shop.app.api.response.ProductsResponse;
+import shop.woowasap.shop.app.api.response.ProductsResponse.ProductResponse;
 import shop.woowasap.shop.app.product.Product;
 import shop.woowasap.shop.app.api.request.RegisterProductRequest;
 import shop.woowasap.shop.app.api.request.UpdateProductRequest;
@@ -14,6 +18,8 @@ public class ProductFixture {
     public static final long QUANTITY = 10L;
     public static final LocalDateTime START_TIME = LocalDateTime.of(2023, 8, 5, 12, 30);
     public static final LocalDateTime END_TIME = LocalDateTime.of(2023, 8, 5, 14, 30);
+    public static final int PAGE = 1;
+    public static final int TOTAL_PAGE = 1;
 
     public static Product.ProductBuilder productBuilder(final Long id) {
         return Product.builder()
@@ -32,5 +38,18 @@ public class ProductFixture {
 
     public static UpdateProductRequest updateProductRequest() {
         return new UpdateProductRequest(NAME, DESCRIPTION, PRICE, QUANTITY, START_TIME, END_TIME);
+    }
+
+    public static ProductsResponse productsResponse(final List<Product> products) {
+        final List<ProductResponse> productResponses = products.stream()
+            .map(product -> new ProductResponse(
+                product.getId(),
+                product.getName().getValue(),
+                product.getPrice().getValue().toString(),
+                LocalDateTime.ofInstant(product.getStartTime(), ZoneId.of("Asia/Seoul")),
+                LocalDateTime.ofInstant(product.getEndTime(), ZoneId.of("Asia/Seoul"))
+            )).toList();
+
+        return new ProductsResponse(productResponses, PAGE, TOTAL_PAGE);
     }
 }
