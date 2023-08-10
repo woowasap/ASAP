3ffba2018944114b133006f233e8c635cf67fa28
pushedrumex <pushedrumex@naver.com>
2023-08-10 19:04:27 +0900

    refactor: 상품 전체 조회 기능 관련 Mock 데이터를 수저한다

--- shop/shop-repository/src/main/java/shop/woowasap/shop/repository/MockProductRepository.java
+++ shop/shop-repository/src/main/java/shop/woowasap/shop/repository/MockProductRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
 import shop.woowasap.shop.app.product.Product;
+import shop.woowasap.shop.app.product.Product.ProductBuilder;
 import shop.woowasap.shop.app.spi.ProductRepository;
 import shop.woowasap.shop.app.spi.response.ProductsPaginationResponse;
 
@@ -18,7 +19,8 @@ public class MockProductRepository implements ProductRepository {
     public static final long QUANTITY = 10L;
     public static final LocalDateTime START_TIME = LocalDateTime.of(2023, 8, 5, 12, 30);
     public static final LocalDateTime END_TIME = LocalDateTime.of(2023, 8, 5, 14, 30);
-    public static final LocalDateTime INFINITE_TIME = LocalDateTime.of(9999, 12, 31, 23, 59);
+    public static final LocalDateTime INFINITE_START_TIME = LocalDateTime.of(99_999, 12, 31, 23, 59);
+    public static final LocalDateTime INFINITE_END_TIME = LocalDateTime.of(999_999, 12, 31, 23, 59);
 
     @Override
     public Product persist(final Product product) {
@@ -42,24 +44,15 @@ public class MockProductRepository implements ProductRepository {
 
     @Override
     public ProductsPaginationResponse findAllValidWithPagination(int page, int size) {
-        Product product1 = Product.builder()
+        ProductBuilder productBuilder = Product.builder()
             .id(1L)
             .name(NAME)
             .price(PRICE)
             .quantity(QUANTITY)
-            .startTime(INFINITE_TIME.toInstant(ZoneOffset.UTC))
-            .endTime(INFINITE_TIME.toInstant(ZoneOffset.UTC))
-            .build();
+            .startTime(INFINITE_START_TIME.toInstant(ZoneOffset.UTC))
+            .endTime(INFINITE_END_TIME.toInstant(ZoneOffset.UTC));
 
-        Product product2 = Product.builder()
-            .id(2L)
-            .name(NAME)
-            .price(PRICE)
-            .quantity(QUANTITY)
-            .startTime(INFINITE_TIME.toInstant(ZoneOffset.UTC))
-            .endTime(INFINITE_TIME.toInstant(ZoneOffset.UTC))
-            .build();
-
-        return new ProductsPaginationResponse(List.of(product1, product2), 1, 1);
+        List<Product> products = List.of(productBuilder.id(1L).build(), productBuilder.id(2L).build());
+        return new ProductsPaginationResponse(products, 1, 1);
     }
 }
