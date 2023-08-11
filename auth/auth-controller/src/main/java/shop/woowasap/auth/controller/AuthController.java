4503ce19fe34cecd650dfd5b4c44229c5557584f
김명준 <audwnswmf@naver.com>
2023-08-11 18:15:57 +0900

    feat: 컨트롤러용 dto 생성

--- auth/auth-controller/src/main/java/shop/woowasap/auth/controller/AuthController.java
+++ auth/auth-controller/src/main/java/shop/woowasap/auth/controller/AuthController.java
@@ -1,18 +1,23 @@
 package shop.woowasap.auth.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.woowasap.auth.controller.request.SignUpRequest;
 import shop.woowasap.auth.domain.exception.AuthDomainBaseException;
 import shop.woowasap.auth.domain.exception.DuplicatedUserIdException;
 import shop.woowasap.auth.domain.in.UserUseCase;
 import shop.woowasap.auth.domain.in.request.UserCreateRequest;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
@@ -21,18 +26,43 @@ public class AuthController {
     private final UserUseCase userUseCase;
 
     @PostMapping("/signup")
-    public ResponseEntity<Void> signUp(@RequestBody UserCreateRequest userCreateRequest) {
-        userUseCase.createUser(userCreateRequest);
+    public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
+
+        userUseCase.createUser(
+            new UserCreateRequest(signUpRequest.userId(), signUpRequest.password()));
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @ExceptionHandler(DuplicatedUserIdException.class)
-    public ResponseEntity<String> handleAuthExceptions(DuplicatedUserIdException exception) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    public ResponseEntity<String> handleAuthExceptions(
+        DuplicatedUserIdException duplicatedUserIdException) {
+
+        log.info(duplicatedUserIdException.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+            .body(duplicatedUserIdException.getMessage());
     }
 
     @ExceptionHandler(AuthDomainBaseException.class)
-    public ResponseEntity<String> handleAuthExceptions(AuthDomainBaseException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    public ResponseEntity<String> handleAuthExceptions(
+        AuthDomainBaseException authDomainBaseException) {
+
+        log.info(authDomainBaseException.getMessage());
+        return ResponseEntity.badRequest().body(authDomainBaseException.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException methodArgumentNotValidException) {
+
+        String defaultMessage = methodArgumentNotValidException
+            .getBindingResult()
+            .getAllErrors()
+            .stream()
+            .findFirst()
+            .get()
+            .getDefaultMessage();
+        log.info(defaultMessage);
+        return ResponseEntity.badRequest()
+            .body(defaultMessage);
     }
 }
