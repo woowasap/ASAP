332e4333e547e614d74dd99c0cce3bd34af685ea
grolarkim <audwnswmf@naver.com>
2023-08-14 16:46:07 +0900

    feat: 회원 가입 컨트롤러 (#87)

--- auth/auth-service/src/main/java/shop/woowasap/auth/service/configuration/PasswordEncoderConfiguration.java
+++ auth/auth-service/src/main/java/shop/woowasap/auth/service/configuration/PasswordEncoderConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfiguration {
+public class PasswordEncoderConfiguration {
 
     @Bean
     public PasswordEncoder bcryptPasswordEncoder() {
