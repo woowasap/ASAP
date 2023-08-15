841355c611fc3fc29479f476c3235a7d65dbb3de
xb205 <62425964+devxb@users.noreply.github.com>
2023-08-15 10:50:37 +0900

    feat: Order 도메인에 createdAt 추가 (#111)

--- order/order-domain/src/test/java/shop/woowasap/order/domain/OrderTest.java
+++ order/order-domain/src/test/java/shop/woowasap/order/domain/OrderTest.java
@@ -34,7 +34,9 @@ class OrderTest {
                     .build();
 
             // then
-            assertThat(order).usingRecursiveComparison().isEqualTo(expected);
+            assertThat(order).usingRecursiveComparison()
+                .ignoringFields("createdAt")
+                .isEqualTo(expected);
         }
 
         @Test
