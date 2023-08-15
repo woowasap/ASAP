1283fdddccf79e535af4c598d2cf1592ad33c992
ProtoSeo <proto_seo@naver.com>
2023-08-15 19:07:32 +0900

    feat: 장바구니 상품 등록 도메인 기능 구현

--- shop/shop-domain/src/main/java/shop/woowasap/shop/domain/cart/Cart.java
+++ shop/shop-domain/src/main/java/shop/woowasap/shop/domain/cart/Cart.java
@@ -2,7 +2,9 @@ package shop.woowasap.shop.domain.cart;
 
 import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class Cart {
 
     private final Long id;
@@ -14,5 +16,23 @@ public class Cart {
         this.id = id;
         this.userId = userId;
         this.cartProducts = cartProducts;
+    }
+
+    public void addProduct(final CartProduct cartProduct) {
+        if (hasCartProduct(cartProduct)) {
+            updateCartProductInCartProducts(cartProduct);
+            return;
+        }
+        cartProducts.add(cartProduct);
+    }
+
+    private void updateCartProductInCartProducts(final CartProduct cartProduct) {
+        final CartProduct updatedCartProduct = cartProduct.addQuantity(cartProduct.getQuantity());
+        cartProducts.remove(cartProduct);
+        cartProducts.add(updatedCartProduct);
+    }
+
+    private boolean hasCartProduct(final CartProduct cartProduct) {
+        return cartProducts.stream().anyMatch(cp -> cp == cartProduct);
     }
 }
---
+++ shop/shop-domain/src/test/java/shop/woowasap/shop/domain/support/CartFixture.java
@@ -0,0 +1,25 @@
+package shop.woowasap.shop.domain.support;
+
+import java.util.ArrayList;
+import shop.woowasap.shop.domain.cart.Cart;
+import shop.woowasap.shop.domain.cart.CartProduct;
+import shop.woowasap.shop.domain.cart.CartProductQuantity;
+
+public final class CartFixture {
+
+    private CartFixture() {
+    }
+
+    public static Cart.CartBuilder getEmptyCartBuilder() {
+        return Cart.builder()
+            .id(1L)
+            .userId(1L)
+            .cartProducts(new ArrayList<>());
+    }
+
+    public static CartProduct.CartProductBuilder getCartProductBuilder() {
+        return CartProduct.builder()
+            .product(DomainFixture.getDefaultBuilder().build())
+            .quantity(new CartProductQuantity(10L));
+    }
+}
