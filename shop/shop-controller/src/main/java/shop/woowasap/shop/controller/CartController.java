6bbcc781b858af9474f302ddee50de47de28d2eb
pushedrumex <pushedrumex@naver.com>
2023-08-16 01:02:26 +0900

    feat: 장바구니 상품 제거 컨트롤러를 생성한다

--- shop/shop-controller/src/main/java/shop/woowasap/shop/controller/CartController.java
+++ shop/shop-controller/src/main/java/shop/woowasap/shop/controller/CartController.java
@@ -2,14 +2,18 @@ package shop.woowasap.shop.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import shop.woowasap.shop.domain.api.cart.CartUseCase;
 import shop.woowasap.shop.domain.api.cart.request.AddCartProductRequest;
 import shop.woowasap.shop.domain.api.cart.response.CartResponse;
+import shop.woowasap.shop.domain.exception.CannotFindProductInCartException;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,4 +37,15 @@ public class CartController {
         return ResponseEntity.ok().build();
     }
 
+    @DeleteMapping
+    public ResponseEntity<Void> deleteCartProduct(@RequestParam("product-id") long productId) {
+        cartUseCase.deleteCartProduct(MOCK_USER_ID, productId);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(CannotFindProductInCartException.class)
+    public ResponseEntity<Void> handleException() {
+        return ResponseEntity.badRequest().build();
+    }
 }
