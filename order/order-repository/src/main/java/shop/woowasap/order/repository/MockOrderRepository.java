ab42ccfeac19a3b3b5d82663b89aa33ec79153b6
devxb <dlwnsdud205@naver.com>
2023-08-15 12:15:06 +0900

    feat: OrderRepository.findAllOrderByUserId 구현

--- order/order-repository/src/main/java/shop/woowasap/order/repository/MockOrderRepository.java
+++ order/order-repository/src/main/java/shop/woowasap/order/repository/MockOrderRepository.java
@@ -1,7 +1,8 @@
 package shop.woowasap.order.repository;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Repository;
 import shop.woowasap.order.domain.Order;
 import shop.woowasap.order.domain.out.OrderRepository;
@@ -10,12 +11,18 @@ import shop.woowasap.order.domain.out.response.OrdersPaginationResponse;
 @Repository
 public class MockOrderRepository implements OrderRepository {
 
+    private final Map<Long, Order> userIdPerOrderRepository = new HashMap<>();
+
     @Override
     public void persist(final Order order) {
+        userIdPerOrderRepository.put(order.getUserId(), order);
     }
 
     @Override
-    public OrdersPaginationResponse findAllOrderByUserId(final long userId, final int page, final int size) {
-        return null;
+    public OrdersPaginationResponse findAllOrderByUserId(final long userId, final int page,
+        final int size) {
+
+        return new OrdersPaginationResponse(List.of(userIdPerOrderRepository.get(userId)), page,
+            size);
     }
 }
