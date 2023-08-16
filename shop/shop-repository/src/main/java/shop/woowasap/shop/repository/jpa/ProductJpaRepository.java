3f19e12273d35abfa6f5ce152ae13929f8ba0b69
pushedrumex <pushedrumex@naver.com>
2023-08-16 21:00:53 +0900

    feat: Product repository 기능을 구현한다

--- shop/shop-repository/src/main/java/shop/woowasap/shop/repository/jpa/ProductJpaRepository.java
+++ shop/shop-repository/src/main/java/shop/woowasap/shop/repository/jpa/ProductJpaRepository.java
@@ -1,7 +1,18 @@
 package shop.woowasap.shop.repository.jpa;
 
+import java.time.Instant;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.woowasap.shop.repository.entity.ProductEntity;
 
 public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
+
+    Optional<ProductEntity> findByIdAndEndTimeAfter(final long productId,
+        final Instant nowTime);
+
+    Page<ProductEntity> findAllByEndTimeAfter(final Instant nowTime, final Pageable pageable);
+
+    Page<ProductEntity> findAll(final Pageable pageable);
 }
