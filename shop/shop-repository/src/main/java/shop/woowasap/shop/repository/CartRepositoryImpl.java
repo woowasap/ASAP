625746e2266852dd221f5f380557a42aa1528880
SeoSeunghun <proto_seo@naver.com>
2023-08-17 11:18:08 +0900

    feat: CartRepository 기능 구현 (#162)

---
+++ shop/shop-repository/src/main/java/shop/woowasap/shop/repository/CartRepositoryImpl.java
@@ -0,0 +1,41 @@
+package shop.woowasap.shop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import shop.woowasap.shop.domain.cart.Cart;
+import shop.woowasap.shop.domain.spi.CartRepository;
+import shop.woowasap.shop.repository.entity.CartEntity;
+import shop.woowasap.shop.repository.jpa.CartJpaRepository;
+
+@Repository
+@RequiredArgsConstructor
+public class CartRepositoryImpl implements CartRepository {
+
+    private final CartJpaRepository cartJpaRepository;
+
+    @Override
+    public Cart createEmptyCart(final long userId, final long cartId) {
+        final CartEntity cartEntity = new CartEntity(cartId, userId);
+        cartJpaRepository.save(cartEntity);
+
+        return cartEntity.toDomain();
+    }
+
+    @Override
+    public Cart persist(final Cart cart) {
+        final CartEntity cartEntity = cartJpaRepository.save(CartEntity.from(cart));
+
+        return cartEntity.toDomain();
+    }
+
+    @Override
+    public boolean existCartByUserId(final long userId) {
+        return cartJpaRepository.existsByUserId(userId);
+    }
+
+    @Override
+    public Cart getByUserId(final long userId) {
+        return cartJpaRepository.findByUserId(userId)
+            .orElseThrow(IllegalStateException::new).toDomain();
+    }
+}
