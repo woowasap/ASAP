d40c43567b7b808d667d6db05557651e29be9bf1
devxb <dlwnsdud205@naver.com>
2023-08-17 20:00:54 +0900

    refactor: Payment.pay 인터페이스 시그니처를 수정한다

--- order/order-domain/src/main/java/shop/woowasap/order/domain/out/Payment.java
+++ order/order-domain/src/main/java/shop/woowasap/order/domain/out/Payment.java
@@ -2,6 +2,6 @@ package shop.woowasap.order.domain.out;
 
 public interface Payment {
 
-    boolean pay(long userId);
+    boolean pay(long userId, final long orderId, final String totalPrice);
 
 }
