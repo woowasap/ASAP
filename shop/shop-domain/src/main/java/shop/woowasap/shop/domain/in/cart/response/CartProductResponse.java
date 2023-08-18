3f9c934f566293a0e52b8486eb4c7cc1b9d45e7e
ProtoSeo <proto_seo@naver.com>
2023-08-19 02:16:03 +0900

    refactor: 매개변수 명 수정

--- shop/shop-domain/src/main/java/shop/woowasap/shop/domain/in/cart/response/CartProductResponse.java
+++ shop/shop-domain/src/main/java/shop/woowasap/shop/domain/in/cart/response/CartProductResponse.java
@@ -1,6 +1,6 @@
 package shop.woowasap.shop.domain.in.cart.response;
 
-public record CartProductResponse(long productId, String name, String price, long purchaseQuantity,
-                                  long remainQuantity) {
+public record CartProductResponse(long productId, String name, String price, long quantity,
+                                  long productRemainQuantity) {
 
 }
